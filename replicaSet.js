26638e84680ac7a0c00363f4c3f3ec589c9925f9
Daniel05B <danielbonillareyes2005@gmail.com>
2024-11-29 23:07:24 -0500

    chore: add database replicate connection

---
+++ replicaSet.js
@@ -0,0 +1,37 @@
+/* Replica Set de los 3 nodos */
+mongod --replSet basketballTournamentRS --dbpath="C:\data\db\nodo1" --port 27017
+mongod --replSet basketballTournamentRS --dbpath="C:\data\db\nodo2" --port 27018
+mongod --replSet basketballTournamentRS --dbpath="C:\data\db\nodo3" --port 27019
+
+/* Conectarse al nodo primario */
+mongosh --host localhost --port 27017
+
+/* Inicializar replica Set */
+rs.initiate({
+    _id: "basketballTournamentRS",
+    members: [
+      {
+        _id: 0,
+        host: "localhost:27017",
+        priority: 3
+      },
+      {
+        _id: 1,
+        host: "localhost:27018",
+        priority: 2,
+      },
+      {
+        _id: 2,
+        host: "localhost:27019",
+        priority: 2,
+      }
+    ],
+    settings: {
+      heartbeatTimeoutSecs: 10,
+      electionTimeoutMillis: 8000,
+      catchUpTimeoutMillis: 30000
+    }
+  })
+
+  /* Verificar estado de la replica Set */
+  rs.status()
